7790eafc4f7680c31e0269f123218790189968de
Akshat-Raii <akshatrai7feb@gmail.com>
2025-09-03 14:09:22 +0530

    Final Commit

--- config/db.py
+++ config/db.py
@@ -1,6 +1,16 @@
+import os
+from dotenv import load_dotenv
 from pymongo import MongoClient
 
+# Load environment variables from .env file
+load_dotenv()
 
-client = MongoClient("mongodb+srv://akshat:akshat0365@memoir.do177tt.mongodb.net/")
+# Get the MongoDB URI from the environment variables
+MONGO_URI = os.getenv("MONGO_URI")
+
+# Connect to the database using the URI
+client = MongoClient(MONGO_URI)
 db = client["notes"]
 collection = db["notes"]
+
+# print("Successfully connected to MongoDB!")
